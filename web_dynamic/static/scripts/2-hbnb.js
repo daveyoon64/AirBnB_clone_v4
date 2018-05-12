91732d9712fd3424f570c0f403cf493c567d9cf9
Dave Yoon <daveyoon64@gmail.com>
2018-05-12 03:38:58 +0000

    Get API server and HBNB server working

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,19 @@
 $(() => {
-  
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if ('status' in data) {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+  $('UL.popover > LI > INPUT').change(() => {
+    let ids = $('UL.popover > LI > INPUT:checkbox:checked').map(function () {
+      return $(this).data('id');
+    }).get(); 
+    let names = $('UL.popover > LI > INPUT:checkbox:checked').map(function () {
+      return $(this).data('name');
+    }).get(); 
+    $('DIV.amenities > h4').text(names.join(', '));    
+    console.log(ids);
+  });
 });
